6eda8baad17b12a6eefe9d5c841ae7830e0cb91f
Harold Varde <harvardii15@gmail.com>
2022-12-02 23:09:14 +0800

    MOD: navbar component; ADD: new mock images

---
+++ components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="fixed h-14 w-full flex flex-nowrap  item-center mb-[2px] p-4 bg-[#121212] z-10">
+      {/* Left Side */}
+      <div className="flex grow items-center justify-"></div>
+      {/* Middle*/}
+      {/* Right Side */}
+    </div>
+  );
+};
+
+export default Navbar;
