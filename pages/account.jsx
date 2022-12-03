140a34421aded45bf0cad1adb0d62e7d238886fa
Harold Varde <harvardii15@gmail.com>
2022-12-03 15:34:29 +0800

    MOD: account page, fix button and alignmet

--- pages/account.jsx
+++ pages/account.jsx
@@ -8,13 +8,22 @@ const account = () => {
   console.log(session);
   if (session) {
     return (
-      <div className="pt-[128px] flex flex-col max-w-[400px] w-full mx-auto p-4">
+      <div className="pt-[128px] flex flex-col items-center max-w-[400px] w-full mx-auto p-4">
+        <div className="pb-4 m-auto">
+          <Image
+            src={session.user.image}
+            alt="/"
+            width="100"
+            height="100"
+            className="rounded-full"
+          />
+        </div>
         <h2 className="text-2xl font-bolf">Welcome, {session.user.name}</h2>
         <p className="py-4">Sign in as {session.user.email}</p>
-        <div className="pb-4 m-auto">
-          <Image src={session.user.image} alt="/" width="100" height="100" />
-        </div>
-        <button onClick={() => signOut()} className="">
+        <button
+          onClick={() => signOut()}
+          className="flex items-center justify-center p-3 bg-gray-600 border-gray-600 rounded-md w-full"
+        >
           Sign out
         </button>
       </div>
