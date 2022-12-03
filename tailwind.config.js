060ecabe4fb864d0c76de699beaf5d304edddc8e
Harold Varde <harvardii15@gmail.com>
2022-12-03 16:14:16 +0800

    MOD: tw config add screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,28 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      xs: "400px",
+      // => @media (min-width: 400px) { ... }
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1028px",
+      // => @media (min-width: 1028px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1600px",
+      // => @media (min-width: 1600px) { ... }
+
+      "3xl": "1800px",
+      // => @media (min-width: 1800px) { ... }
+    },
   },
   plugins: [],
 };
