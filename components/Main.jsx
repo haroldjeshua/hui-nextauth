6bffb687bf99cd0ba19ea807450dd305fdc8eea3
Harold Varde <harvardii15@gmail.com>
2022-12-16 23:54:18 +0800

    ADD: IconBar comp + static img

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Hero from "./Hero";
+import IconBar from "./IconBar";
 import LiveChannels from "./LiveChannels";
 
 const Main = () => {
@@ -7,6 +8,7 @@ const Main = () => {
     <div className="absolute left-[64px] xl:left-[232px] p-4">
       <Hero />
       <LiveChannels />
+      <IconBar />
     </div>
   );
 };
